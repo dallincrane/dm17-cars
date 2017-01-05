ab559c1f5594154818f68d1762ed153d487a62c6
Dallin Crane <dallincrane@gmail.com>
2017-01-05 12:31:58 -0700

    add a cart

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var config = require('./config.json');
 
 var carsController = require('./controllers/cars_controller');
+var cartController = require('./controllers/cart_controller');
 
 var app = express();
 
 app.use(bodyParser.json());
+
+app.use(session({
+  secret: config.sessionSecret,
+  saveUninitialized: false,
+  resave: false
+}));
 
 app.use(function (req, res, next) {
   console.log(req.path);
@@ -18,7 +28,9 @@ app.post('/cars', carsController.create);
 app.put('/cars/:id', carsController.update);
 app.delete('/cars/:id', carsController.destroy);
 
-var port = 3000;
-app.listen(port, function() {
-  console.log('listening to port', port);
+app.get('/cart', cartController.index);
+app.post('/cart', cartController.create);
+
+app.listen(config.port, function() {
+  console.log('listening to port', config.port);
 });
